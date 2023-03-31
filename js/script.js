ffd188c09cdc4f1efbd89e86c0b6c5cc7c6983f9
ridho1610 <98494067+ridho1610@users.noreply.github.com>
2023-04-01 06:15:07 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -21,18 +21,18 @@ window.onscroll = () => {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
-        };
+            });
+        }
     });
 
-// sticky navbar
-let header = document.querySelector('.header');
+    // sticky navbar
+    let header = document.querySelector('.header');
 
-header.classList.toggle('sticky', window.scrollY > 100);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-// remove menu icon navbar  when click navbar link (scroll)
-menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active');
+    // remove menu icon navbar  when click navbar link (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
 //dark light mode
@@ -44,10 +44,13 @@ darkModeIcon.onclick = () => {
 };
 
 ScrollReveal({
-    reset: true,
+    // reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200
 });
 
 ScrollReveal().reveal('.home-content, .heading', {origin : 'top'});
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form' ,{origin : 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img img' ,{origin : 'left'});
+ScrollReveal().reveal('.home-content h3, .home-content p , .about-content' ,{origin : 'right'});
